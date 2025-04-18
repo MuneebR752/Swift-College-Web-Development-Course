58df82a806331c73b37d222ae11ffc2072f268b3
Muneeb <muneeburrehman440@gmail.com>
2025-04-18 18:14:48 +0500

    initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,76 @@
+// var x = 5;
+
+// let y = 10;
+
+// const pi = 3.14;
+
+// var x = 8;
+
+// y = 6;
+
+// pi = 4;
+
+// console.log(pi);
+
+// For Input
+
+// let myName = prompt("What is Your Name");
+
+// document.writeln(myName);
+
+// let x;
+
+// x = 5;
+
+// document.writeln(x + "<br>");
+
+// x = x + 2;
+
+// document.writeln(x + "<br>");
+
+// x = x + 8; same as below
+
+// x += 8;
+
+// document.writeln(x + "<br>");
+
+// let x = 5;
+// let y = 6;
+
+// let z = x + y;
+
+// document.writeln(z + "<br>");
+
+// let a = 5 > 6;
+
+// document.writeln(a + "<br>" + typeof a);
+// let name1 = prompt("What's Your Name ");
+// let age = prompt("Enter Your Age ");
+
+// if (age >= 20 && name1 == "ali") {
+//   document.writeln("Your name is ali and age greater than 20");
+// } else {
+//   document.writeln("Your name is not ali or age less than 20");
+// }
+
+// if (age >= 20 || name1 == "ali") {
+//   document.writeln("Your name is ali and age greater than 20");
+// } else {
+//   document.writeln("Your name is not ali or age less than 20");
+// }
+
+let num = prompt("Enter a Number");
+
+// if (num >= 0) {
+//   document.writeln("Number is Positive");
+// } else {
+//   document.writeln("Number is Negative");
+// }
+
+// if (num % 2 == 0) {
+//   document.writeln("Number is Even");
+// } else {
+//   document.writeln("Number is Odd");
+// }
+
+
